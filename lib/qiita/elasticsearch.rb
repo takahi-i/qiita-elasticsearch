1b592272ec2776eb4ff2d13596b8eeeca8bc6918
Ryo Nakamura <r7kamura@gmail.com>
2015-03-13 11:45:30 +0900

    bundle gem qiita-elasticsearch

---
+++ lib/qiita/elasticsearch.rb
@@ -0,0 +1,7 @@
+require "qiita/elasticsearch/version"
+
+module Qiita
+  module Elasticsearch
+    # Your code goes here...
+  end
+end
---
+++ lib/qiita/elasticsearch/version.rb
@@ -0,0 +1,5 @@
+module Qiita
+  module Elasticsearch
+    VERSION = "0.1.0"
+  end
+end
