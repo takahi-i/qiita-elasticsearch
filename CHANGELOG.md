548eed8632f8e19901434958ae18df2461ab8409
Ryo Nakamura <r7kamura@gmail.com>
2015-03-16 16:38:43 +0900

    v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.1.2
+- Fix filter query for hierarchal fields
+
 ## 0.1.1
 - Support hierarchal fields
 
--- lib/qiita/elasticsearch/version.rb
+++ lib/qiita/elasticsearch/version.rb
@@ -1,5 +1,5 @@
 module Qiita
   module Elasticsearch
-    VERSION = "0.1.1"
+    VERSION = "0.1.2"
   end
 end
